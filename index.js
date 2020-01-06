83baf32ffdeee0bdb81aad4b11e73db0fff95bb5
Muhammed Saifudeen Salaudeen <mr.salaudeen.official@gmail.com>
2020-01-06 11:24:56 +0100

    Added Kubernetes files

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express')
+const redis = require('redis')
+const process = require('process')
 
 
 const app = express()
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+})
+client.set('visit', 0)
+
 const port = 4444
 
 app.get("/", (req, res) => {
-    res.send({
-        message: "You are welcome to docker!"
+    // process.exit(0)
+    client.get('visit', (err, visit) => {
+        res.send("This site has been visited " + visit + " times.")
+        client.set('visit', parseInt(visit) + 1)
     })
 })
 
